c2747fdb6b040d07fde1f908f7b38c0820468f04
Michael D. Hale <mhale@qualcomm.com>
2016-10-31 12:59:13 -0700

    Convert echo to debug so ssh is happy

--- gpg-functions.sh
+++ gpg-functions.sh
@@ -1,6 +1,6 @@
 export GPG_TTY=$(tty)
 function gpg_setup_env () {
-	echo "Configuring GPG Agent Environment"
+	debug "Configuring GPG Agent Environment"
 	if [ -f "${HOME}/.gpg-agent-info" ]; then
   	. "${HOME}/.gpg-agent-info"
   	export GPG_AGENT_INFO
@@ -17,7 +17,7 @@ function gpg_kill_agent () {
 function gpg_check_agent () {
 	local result
 	gpg_setup_env
-	echo "Checking GPG Agent"
+	debug "Checking GPG Agent"
 	echo "/bye" | tail -n +1 | gpg-connect-agent &>/dev/null
 	result=$?
 	return $result
@@ -25,7 +25,7 @@ function gpg_check_agent () {
 function gpg_start_agent () {
 	local result
 	gpg-agent --daemon --enable-ssh-support           --write-env-file "${HOME}/.gpg-agent-info" &>/dev/null 
-	echo "Starting GPG Agent"
+	debug "Starting GPG Agent"
 	result=$?
 	gpg_setup
 	return  $result
@@ -41,6 +41,4 @@ function gpg_restart () {
 	sleep 1
 	gpg_activate
 }
-#echo "/bye" | gpg-connect-agent  &>/dev/null  || \
-#    gpg-agent --daemon --enable-ssh-support           --write-env-file "${HOME}/.gpg-agent-info" &>/dev/null 
 
